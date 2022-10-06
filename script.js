88cadb7599b1d70575f7d1b85d87a77f19f35fe0
SosoRicsi <98759127+SosoRicsi@users.noreply.github.com>
2022-10-06 10:51:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  var close = document.getElementsByClassName("closebtn");
+  var i;
+  
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+      var div = this.parentElement;
+      div.style.opacity = "0";
+      setTimeout(function(){ div.style.display = "none"; }, 600);
+    }
+  }
